26f8bbdce5c6733b43709c10e1d37a5ac51f05ef
Glow-in-active <timurtimurcik7@gmail.com>
2025-02-12 02:48:19 +0300

    Написал тесты для ручек в постгрес, пока в мейне реализовал завоз данных в таблицу

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"link_shortener/internal/generator"
+	"link_shortener/internal/storage"
+	"log"
 )
 
 func main() {
-	var urll string
-	urll = "github.com/fdsxfsdf/dsfsdfdsf"
-	shortURL := generator.GenerateShortURL(urll)
+	connStr := "postgres://admin:secret@localhost:5432/mydb?sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Ошибка подключения к базе данных: ", err)
+	}
+	var url string
+	fmt.Scan(&url)
+	shortURL := generator.GenerateShortURL(url)
 
-	fmt.Println(shortURL)
+	storage.AddData(db, shortURL, url)
+	data, err := storage.GetData(db)
+	if err != nil {
+		log.Fatal("Ошибка при извлечении данных: ", err)
+	}
+	fmt.Println("Данные из таблицы:", data)
+
 }
